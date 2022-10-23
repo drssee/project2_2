70abb700391fd69af4a2d65645f9917515e69d66
drssee <113401870+drssee@users.noreply.github.com>
2022-10-23 23:27:08 +0900

    댓글+대댓글

--- src/main/java/org/zerock/comment/CommentController.java
+++ src/main/java/org/zerock/comment/CommentController.java
@@ -89,4 +89,33 @@ public class CommentController {
             return new ResponseEntity<String>("MOD_ERR",HttpStatus.BAD_REQUEST);
         }
     }
+
+    //****************댓글 작성하는법****************
+
+    //1.해당 게시글(bno)불러오면서 댓글(해당 게시글 bno에 맞는)리스트도 같이 불러옴
+
+    //2.입력폼에 댓글을 작성하고 전송버튼을 누르면 ajax 메서드가 요청 헤드 post , 요청 바디에 JSON.stringify(js댓글객체) + 쿼리로 bno 을 담아서 http요청 메시지를 보냄
+
+    //3.controller의 postmapping 메서드에서 jackson databind를 이용해서 매개변수@requestbody객체 에 자바 객체로 변환후 전달받고
+
+    //4.해당 메서드에서 객체에 setbno나 setcommenter등 백엔드에서 처리할 작업 + db에 insert 이후
+
+    //5.@responsebody가 붙어있는 해당 메서드에서 리턴한 string->new responseentity<str>(str,상태코드)는 http응답 메시지 바디에 담겨서
+
+    //6.요청을 한 클라이언트에게 도착해서 성공시 댓글 리스트 다시 불러오기 or 실패시 alert
+
+
+
+    //****************대댓글(답글) 작성하는법****************
+
+    //1.대댓글(답글) 작성 전용 폼을 만들어서
+
+    //2.댓글 작성과 비슷하지만 다른점이 , pcno의 값도 가져와 js객체에 담아야 한다는 것과 기존의 댓글을 가져오는 쿼리문을 수정해야함
+
+    //3.selectAll if(pcno=0,cno,pcno) order by pcno , cno + controller의 post method에 넘어온 pcno가 없을시 null이 아닌 0이 되게(구분위함)
+    // pcno값이 없는(0인) 댓글들 <- 부모댓글들은 pcno=cno 값을 넣어주고 pcno , cno 순서로 정렬
+
+    //4.대댓글과 부모댓글 구분을 위해 뷰에 if(pcno!=cno) 조건으로 구분 해줌
+
+    //5.나머지 update delete는 동일
 }
